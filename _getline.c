53e1fa216b82225d57981ca8e58dde58f7020d3c
Arogunmasa <moyosore2019.com>
2022-11-16 12:12:40 -0800

    3885

---
+++ _getline.c
@@ -0,0 +1,22 @@
+#include
+"main.h"
+
+/**
+ *  _getline - gets line of input from the standard input
+ *   
+ *    Return: pointer to the string retrieved from stdin
+ *     */
+char *_getline(void)
+{
+		char *line = NULL;
+		size_t size = 0;
+		if (getline(&line, &size, stdin) == -1)
+		{
+			free(line);							                return (NULL);
+		}
+
+		if (line[strlen(line) - 1] == '\n')
+			line[strlen(line) - 1] = '\0';
+
+		return (line);
+}
